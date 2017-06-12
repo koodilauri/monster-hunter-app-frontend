2fc308c0066c96137bd071cb9156d573d5b45af0
Lauri <lakoivisto@gmail.com>
2017-06-12 15:47:37 +0300

    add listing component

---
+++ src/Listing.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Listing extends React.Component{
+  render(){
+    return(
+      <div>
+        <p>hei</p>
+      </div>
+    )
+  }  
+}
+
+export default Listing;
