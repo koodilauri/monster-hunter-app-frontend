81fc00bbb1b31b702e470ae2b9943f1999f6c210
Lauri <lakoivisto@gmail.com>
2017-06-29 20:44:57 +0300

    edit css and form

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import SubmissionForm from "../submission/SubmissionForm";
@@ -13,10 +12,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
+        <div><h1>Hunters log</h1></div>
         <SubmissionForm store={store}/>        
       </div>
     );
