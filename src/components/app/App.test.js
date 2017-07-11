183ecad3a63548bc959d3412e85f6ca80159f86d
Lauri <lakoivisto@gmail.com>
2017-07-11 18:39:24 +0300

    edit app.test.js

--- src/components/app/App.test.js
+++ src/components/app/App.test.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import configureStore from '../../store'
+import store from '../../store'
 import App from './App'
-const store = configureStore()
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
