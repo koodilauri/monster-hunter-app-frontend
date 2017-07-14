633fe815d768f1bcb34b5f448defa8f4622f2b84
Lauri <lakoivisto@gmail.com>
2017-07-14 14:36:33 +0300

    edit store

--- src/store.js
+++ src/store.js
@@ -3,17 +3,20 @@ import { createStore, applyMiddleware } from 'redux'
 import createSagaMiddleware from "redux-saga"
 import rootSaga from "./sagas"
 
-const sagaMiddleware = createSagaMiddleware()
+const configureStore = () => {
+  const sagaMiddleware = createSagaMiddleware()
 
-function configureStore() {
-  return createStore(
+  const createStoreWithMiddleware = applyMiddleware(sagaMiddleware)(createStore)
+
+  const store = createStoreWithMiddleware(
     reducers,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    applyMiddleware(sagaMiddleware)
-  )  
+  )
+
+  sagaMiddleware.run(rootSaga)
+
+  return store
 }
-const store = configureStore()
 
-sagaMiddleware.run(rootSaga)
 
-export default store
+export default configureStore()
